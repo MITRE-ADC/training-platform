2c59944b97c5ad3a4547d70550ebaec4088b5cf8
Andallfor <leozwang2005@gmail.com>
2024-10-07 20:11:42 -0400

    Install ShadCN and Remix Icon

---
+++ src/app/dev/layout.tsx
@@ -0,0 +1,16 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+
+export const metadata: Metadata = {
+    title: "UI Component Test"
+  };
+  
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
+    return (
+        <html lang="en">
+            <body>
+                {children}
+            </body>
+        </html>
+    );
+}
