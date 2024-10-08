7a0b4183d45044bd786375ae3b9bebec2aca38bc
Andallfor <leozwang2005@gmail.com>
2024-10-07 20:12:43 -0400

    Linter

--- src/app/dev/layout.tsx
+++ src/app/dev/layout.tsx
@@ -2,15 +2,15 @@ import type { Metadata } from "next";
 import "@/app/globals.css";
 
 export const metadata: Metadata = {
-    title: "UI Component Test"
-  };
-  
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
-    return (
-        <html lang="en">
-            <body>
-                {children}
-            </body>
-        </html>
-    );
+  title: "UI Component Test",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
