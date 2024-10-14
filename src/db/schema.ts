1d990a98c1f53615039a3e8adf7a56b0ea78657c
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-10-14 12:16:10 -0400

    Refactor user comparator

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,16 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
-export const usersTable = pgTable("users", {
+import {eq, or } from "drizzle-orm"
+
+export interface User {
+  id: number;
+  email: string;
+  first_name: string;
+  last_name: string;
+}
+
+export const locateUser = (user: User) => or(eq(users.id, user.id), eq(users.email, user.email))
+
+export const users = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   first_name: varchar({ length: 255 }).notNull(),
   last_name: varchar({ length: 255 }).notNull(),
