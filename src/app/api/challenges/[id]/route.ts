e925df235b88b157c524e20d0fddcad60994e0be
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-10-17 17:37:31 -0400

    Change API route placeholders for schema plan

--- src/app/api/challenges/[id]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-// GET challenge information
-export async function GET(request: NextRequest) {
-  await request.json();
-  return NextResponse.json({ msg: "Hello from server" });
-}
--- src/app/api/grades/[id]/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextRequest, NextResponse } from "next/server";
-
-// GET user information
-export async function GET(request: NextRequest) {
-  await request.json();
-  return NextResponse.json({ msg: "Hello from server" });
-}
