f1d99fa1efc7487c4aaa19b3976961975a540b95
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-11-01 12:47:33 -0400

    Refactor api routes for build acceptance

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -3,36 +3,7 @@ import { HttpStatusCode } from "axios";
 import { locateUser, User, users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { db } from "@/db";
-
-export async function processCreateUserRequest(request: NextRequest) {
-  const user_email = request.nextUrl.searchParams?.get("user_email");
-  console.log(user_email);
-
-  // TODO: password: special finagling with auth
-  // Pick out (only) initialization data for use
-  const user: Pick<User, "email" | "first_name" | "last_name"> =
-    (await request.json()) || { email: user_email };
-  console.log(user);
-
-  return await db
-    .insert(users)
-    .values(user)
-    .returning()
-    .catch((reason) =>
-      NextResponse.json(
-        {
-          message: `Error inserting user ${user} into database: \n\t${reason}`,
-        },
-        { status: HttpStatusCode.InternalServerError }
-      )
-    )
-    .then((user) =>
-      NextResponse.json(
-        { message: `Succesfully inserted: \n\t${user}` },
-        { status: HttpStatusCode.Created }
-      )
-    );
-}
+import { processCreateUserRequest } from "../../util";
 
 // Get data for a single user -- detailed
 export async function GET(request: NextRequest) {
