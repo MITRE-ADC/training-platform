2c52b1563515a48e6cf48b093f8c0756f2fa231c
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-11-02 14:26:38 -0400

    GET all user_courses, user_assignments

--- src/app/api/user_courses/route.ts
+++ src/app/api/user_courses/route.ts
@@ -4,17 +4,25 @@ import { locateUser, User, users } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { db } from "@/db";
 import { processLinkCourseRequest } from "../util";
+import { getAllUserCourses } from "@/db/queries";
 
 // Get data for a single user -- detailed
-export async function GET(request: NextRequest) {
-  // using email as the primary key
-  const user_email = request.nextUrl.searchParams?.get("user_email");
-  console.log(user_email);
-
-  return NextResponse.json(
-    { message: "Not Implemented" },
-    { status: HttpStatusCode.NotImplemented }
-  );
+export async function GET() {
+  try {
+    return NextResponse.json(
+      { data: await getAllUserCourses() },
+      { status: HttpStatusCode.Ok }
+    );
+  } catch (ex) {
+    return NextResponse.json(
+      {
+        message: `Error: ${ex}\n`,
+      },
+      {
+        status: HttpStatusCode.InternalServerError,
+      }
+    );
+  }
 }
 
 // Create new user
