6331dfce02915b8951f03b4fb3ab8fd612a01825
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-11-03 15:35:35 -0500

    POST api/user_courses creates user_course record
    Checks for user, course existance

--- src/app/api/util.ts
+++ src/app/api/util.ts
@@ -11,10 +11,11 @@ import {
 } from "@/db/schema";
 import {
   courseNameExists,
-  userExists,
+  userIdExists,
   getCourse,
   addUserCourse,
   addCourse,
+  courseIdExists,
 } from "@/db/queries";
 
 function error(message: string, status: number = HttpStatusCode.BadRequest) {
@@ -27,6 +28,8 @@ function error(message: string, status: number = HttpStatusCode.BadRequest) {
 export async function processLinkCourseRequest(request: NextRequest) {
   const user_id = request.nextUrl.searchParams?.get("user_id");
   const course_id = request.nextUrl.searchParams?.get("course_id");
+  const assigned_date = request.nextUrl.searchParams?.get("assigned_date");
+  const due_date = request.nextUrl.searchParams?.get("assigned_date");
 
   let body: AddUserCourse | undefined = undefined;
   try {
@@ -42,8 +45,15 @@ export async function processLinkCourseRequest(request: NextRequest) {
 
   const _user_id = body?.user_id ?? parseInt(user_id!);
   const _course_id = body?.course_id ?? parseInt(course_id!);
+  const _assigned_date =
+    body?.assigned_date ??
+    ((assigned_date && new Date(assigned_date!)) || new Date());
+  const _due_date =
+    body?.due_date ?? ((due_date && new Date(due_date!)) || new Date());
 
-  if (!userExists(_user_id)) return error("User not found");
+  if (!(await userIdExists(_user_id))) return error("User not found");
+
+  if (!(await courseIdExists(_course_id))) return error("Course not found");
 
   if (_course_id == -1 || !getCourse(_course_id))
     return NextResponse.json(
@@ -59,8 +69,8 @@ export async function processLinkCourseRequest(request: NextRequest) {
     user_id: _user_id,
     course_id: _course_id,
     course_status: "Not Started",
-    assigned_date: new Date(),
-    due_date: new Date(),
+    assigned_date: _assigned_date,
+    due_date: _due_date,
   });
   return NextResponse.json({ data: first }, { status: HttpStatusCode.Created });
 }
