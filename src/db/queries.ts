02ce0c90078dd8fe2adce21901ea73a0fba6895c
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-11-03 15:34:35 -0500

    Fix exists utilities for correctness

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -38,17 +38,94 @@ export async function deleteUser(id: number) {
   return await db.delete(users).where(eq(users.user_id, id));
 }
 
-export async function userExists(id: number) {
-  return (await db.$count(users, eq(users.user_id, id))) > 1;
+export async function userIdExists(id: number) {
+  return (
+    (await db.$count(db.select().from(users).where(eq(users.user_id, id)))) > 0
+  );
+}
+export async function userNameExists(name: string) {
+  return (
+    (await db.$count(db.select().from(users).where(eq(users.name, name)))) > 0
+  );
 }
 
-export async function courseExists(id: number) {
-  return (await db.$count(courses, eq(courses.course_id, id))) > 1;
+export async function courseIdExists(id: number) {
+  return (
+    (await db.$count(
+      db.select().from(courses).where(eq(courses.course_id, id))
+    )) > 0
+  );
 }
 
 export async function courseNameExists(name: string) {
   // https://orm.drizzle.team/docs/query-utils
-  return (await db.$count(courses, eq(courses.course_name, name))) > 1;
+  return (
+    (await db.$count(
+      db.select().from(courses).where(eq(courses.course_name, name))
+    )) > 0
+  );
+}
+
+export async function userCourseIdExists(id: number) {
+  return (
+    (await db.$count(
+      db.select().from(user_courses).where(eq(user_courses.user_course_id, id))
+    )) > 0
+  );
+}
+
+export async function userCourseExists(course_id: number, user_id: number) {
+  return (
+    (await db.$count(
+      db
+        .select()
+        .from(user_courses)
+        .where(
+          and(
+            eq(user_courses.course_id, course_id),
+            eq(user_courses.user_id, user_id)
+          )
+        )
+    )) > 0
+  );
+}
+
+export async function assignmentIdExists(id: number) {
+  return (
+    (await db.$count(
+      db.select().from(assignments).where(eq(assignments.assignment_id, id))
+    )) > 0
+  );
+}
+
+export async function userAssignmentIdExists(id: number) {
+  return (
+    (await db.$count(
+      db
+        .select()
+        .from(user_assignments)
+        .where(eq(user_assignments.user_assignment_id, id))
+    )) > 0
+  );
+}
+
+export async function userAssignmentExists(
+  assignment_id: number,
+  user_id: number
+) {
+  return (
+    (await db.$count(
+      db
+        .select()
+        .from(user_assignments)
+        .where(
+          and(
+            eq(user_assignments.assignment_id, assignment_id),
+            eq(user_assignments.user_id, user_id)
+          )
+        )
+    )) > 0
+  );
 }
 
 // Courses
