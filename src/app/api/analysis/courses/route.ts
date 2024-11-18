ef3aef2568091184c03c448a30a5fb17a05897ff
Anvay Panguluri <aspanguluri@gmail.com>
2024-11-17 20:56:33 -0500

    fixed pnpm build error

--- src/app/api/analysis/courses/route.ts
+++ src/app/api/analysis/courses/route.ts
@@ -1,71 +1,10 @@
 import { aggregateUserCoursesStatusByUser } from "@/db/queries";
 import { HttpStatusCode } from "axios";
-import { NextResponse } from "next/dist/server/web/spec-extension/response";
+import { NextResponse } from "next/server"
 
 // return number of completed/in progress/not started for every user
 export async function GET() {
-  const data = await aggregateUserCoursesStatusByUser();
-
-  interface Analysis {
-    completed: number;
-    in_progress: number;
-    not_started: number;
-  }
-
-  interface Entry {
-    user_id: number;
-    analysis: Analysis;
-  }
-
-  const res: Entry[] = [];
-  let lastId: number | null = null;
-  data.forEach((element) => {
-    if (Number(element["user_id"]) != lastId) {
-      lastId = Number(element["user_id"]);
-      const newEntry = {
-        user_id: Number(element["user_id"]),
-        analysis: {
-          completed: 0,
-          in_progress: 0,
-          not_started: 0,
-        },
-      };
-
-      switch (element["course_status"]) {
-        case "Completed": {
-          newEntry["analysis"]["completed"] = Number(element["count"]);
-          break;
-        }
-        case "In Progress": {
-          newEntry["analysis"]["in_progress"] = Number(element["count"]);
-          break;
-        }
-        case "Not Started": {
-          newEntry["analysis"]["not_started"] = Number(element["count"]);
-          break;
-        }
-      }
-
-      res.push(newEntry);
-    } else {
-      const entry = res[res.length - 1];
-
-      switch (element["course_status"]) {
-        case "Completed": {
-          entry["analysis"]["completed"] = Number(element["count"]);
-          break;
-        }
-        case "In Progress": {
-          entry["analysis"]["in_progress"] = Number(element["count"]);
-          break;
-        }
-        case "Not Started": {
-          entry["analysis"]["not_started"] = Number(element["count"]);
-          break;
-        }
-      }
-    }
-  });
+  const res = await aggregateUserCoursesStatusByUser();
 
   return NextResponse.json({ data: res }, { status: HttpStatusCode.Ok });
 }
--- src/db/queries.ts
+++ src/db/queries.ts
@@ -101,7 +101,7 @@ export async function userCourseExists(course_id: number, user_id: number) {
 }
 
 export async function aggregateUserCoursesStatusByUser() {
-  return (
+  const data = (
     await db.execute(
       `
       SELECT user_id, course_status, CAST(COUNT(*) AS int) 
@@ -111,6 +111,69 @@ export async function aggregateUserCoursesStatusByUser() {
       `
     )
   ).rows;
+
+  interface Analysis {
+    completed: number;
+    in_progress: number;
+    not_started: number;
+  }
+
+  interface Entry {
+    user_id: number;
+    analysis: Analysis;
+  }
+
+  const res: Entry[] = [];
+  let lastId: number | null = null;
+  data.forEach((element) => {
+    if (Number(element["user_id"]) != lastId) {
+      lastId = Number(element["user_id"]);
+      const newEntry = {
+        user_id: Number(element["user_id"]),
+        analysis: {
+          completed: 0,
+          in_progress: 0,
+          not_started: 0,
+        },
+      };
+
+      switch (element["course_status"]) {
+        case "Completed": {
+          newEntry["analysis"]["completed"] = Number(element["count"]);
+          break;
+        }
+        case "In Progress": {
+          newEntry["analysis"]["in_progress"] = Number(element["count"]);
+          break;
+        }
+        case "Not Started": {
+          newEntry["analysis"]["not_started"] = Number(element["count"]);
+          break;
+        }
+      }
+
+      res.push(newEntry);
+    } else {
+      const entry = res[res.length - 1];
+
+      switch (element["course_status"]) {
+        case "Completed": {
+          entry["analysis"]["completed"] = Number(element["count"]);
+          break;
+        }
+        case "In Progress": {
+          entry["analysis"]["in_progress"] = Number(element["count"]);
+          break;
+        }
+        case "Not Started": {
+          entry["analysis"]["not_started"] = Number(element["count"]);
+          break;
+        }
+      }
+    }
+  });
+
+  return res;
 }
 
 export async function assignmentIdExists(id: number) {
