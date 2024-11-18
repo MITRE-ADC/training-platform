d8a5ec8e6b26789acfb695f725987e00018dba6c
gsarav <gitikasarav@gmail.com>
2024-11-17 19:37:35 -0500

    Recommenting status

--- src/app/api/filters/route.ts
+++ src/app/api/filters/route.ts
@@ -1,27 +1,30 @@
 import { NextRequest, NextResponse } from "next/server";
 import { HttpStatusCode } from "axios";
-import { assignments, courses, users, user_assignments, user_courses } from "@/db/schema";
-import { ConsoleLogWriter, eq, inArray, and } from "drizzle-orm";
+import {
+  assignments,
+  courses,
+  users,
+  user_assignments,
+  user_courses,
+} from "@/db/schema";
+import { inArray, and, sql } from "drizzle-orm";
 import { db } from "@/db";
 
 // Main API endpoint for filtering on admin dash
 // example URL: localhost:3000/api/filters?query="John Doe"&course_filters="Course 4,Course3"&status_filters="In Progress"
-export async function GET(request: NextRequest){
-    // const search_strings = request.nextUrl.searchParams?.get("search_string")?.split(',') || []; // NOT SURE HOW TO IMPLEMENT---> ASK LATER
-    const course_filter = request.nextUrl.searchParams?.get("course_filter")?.split(',') || [];
-    const assignment_filter = request.nextUrl.searchParams?.get("assignment_filter")?.split(',') || [];
-    // const status_filter = request.nextUrl.searchParams?.get("status_filter")?.split(',') || [];
+export async function GET(request: NextRequest) {
+  // const search_strings = request.nextUrl.searchParams?.get("search_string")?.split(',') || []; // NOT SURE HOW TO IMPLEMENT
+  const course_filter =
+    request.nextUrl.searchParams?.get("course_filter")?.split(",") || [];
+  const assignment_filter =
+    request.nextUrl.searchParams?.get("assignment_filter")?.split(",") || [];
+  //const status_filter = request.nextUrl.searchParams?.get("status_filter")?.split(',') || [];
 
-    console.log(course_filter);
-    console.log(assignment_filter);
-    console.log(status_filter);
-
-    if (course_filter==null || assignment_filter == null || status_filter == null){
-      return NextResponse.json({ status: HttpStatusCode.NotFound }); 
-    }
+  console.log(course_filter);
+  console.log(assignment_filter);
+  //console.log(status_filter);
 
   let query;
-
   const conditions = [];
   if (course_filter.length > 0) {
     const courseIds = await db
@@ -34,7 +37,11 @@ export async function GET(request: NextRequest){
     const userCourses = await db
       .select({ user_id: user_courses.user_id })
       .from(user_courses)
-      .where(inArray(user_courses.course_id, filteredCourseIds))
+      .where(inArray(user_courses.course_id, filteredCourseIds)) // Users taking any of the specified courses
+      .groupBy(user_courses.user_id)
+      .having(
+        sql`COUNT(DISTINCT ${user_courses.course_id}) = ${filteredCourseIds.length}`
+      )
       .execute();
 
     const filteredUserIdsForCourses = userCourses.map((row) => row.user_id);
@@ -54,9 +61,15 @@ export async function GET(request: NextRequest){
       .select({ user_id: user_assignments.user_id })
       .from(user_assignments)
       .where(inArray(user_assignments.assignment_id, filteredAssignmentIds))
-      .execute();
+      .groupBy(user_assignments.user_id)
+        .having(
+          sql`COUNT(DISTINCT ${user_assignments.assignment_id}) = ${filteredAssignmentIds.length}`
+        ) 
+        .execute();
 
-    const filteredUserIdsForAssignments = userAssignments.map((row) => row.user_id);
+    const filteredUserIdsForAssignments = userAssignments.map(
+      (row) => row.user_id
+    );
     conditions.push(inArray(users.user_id, filteredUserIdsForAssignments));
   }
 
@@ -81,16 +94,23 @@ export async function GET(request: NextRequest){
   } */
 
   if (conditions.length > 0) {
-    query = db.select().from(users).where(and(...conditions));
+    query = db
+      .select()
+      .from(users)
+      .where(and(...conditions));
   }
 
-  if (query === undefined){
+  if (query === undefined) {
     const usersResult = db.select().from(users).execute();
-    return NextResponse.json({data: usersResult }, {status: HttpStatusCode.Ok});
+    return NextResponse.json(
+      { data: usersResult },
+      { status: HttpStatusCode.Ok }
+    );
   }
 
   const usersResult = await query.execute();
-  return NextResponse.json({ data: usersResult }, { status: HttpStatusCode.Ok }); 
-
+  return NextResponse.json(
+    { data: usersResult },
+    { status: HttpStatusCode.Ok }
+  );
 }
-
