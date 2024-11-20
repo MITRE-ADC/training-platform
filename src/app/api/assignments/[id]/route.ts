92fcb0371acfc637570458e7b2139994a2ec2f38
Anvay Panguluri <aspanguluri@gmail.com>
2024-11-20 00:20:01 -0500

    moving function across files

--- src/app/api/assignments/[id]/route.ts
+++ src/app/api/assignments/[id]/route.ts
@@ -8,7 +8,6 @@ import {
 import { NextRequest, NextResponse } from "next/server";
 import { HttpStatusCode } from "axios";
 import { error } from "../../util";
-import { CHECK_UNAUTHORIZED } from "../../auth";
 
 // GET assignment info
 export async function GET(
@@ -24,47 +23,6 @@ export async function GET(
     return NextResponse.json(
       { data: await getAssignment(id) },
       { status: HttpStatusCode.Ok }
-    );
-  } catch (ex) {
-    return NextResponse.json(
-      {
-        message: `Error: ${ex}\n`,
-      },
-      {
-        status: HttpStatusCode.InternalServerError,
-      }
-    );
-  }
-}
-
-export async function DELETE(
-  request: NextRequest,
-  context: { params: Promise<{ id: string }> }
-) {
-  try {
-    const user_id = (await context.params).id;
-    const err = await CHECK_UNAUTHORIZED(user_id);
-    if (err) return err;
-
-    const { assignment_id } = await request.json();
-    if (!assignment_id) {
-      return NextResponse.json(
-        {
-          error: "Missing Assignment ID",
-        },
-        {
-          status: HttpStatusCode.BadRequest,
-        }
-      );
-    }
-
-    return NextResponse.json(
-      {
-        data: deleteAssignmentForUser(user_id, assignment_id),
-      },
-      {
-        status: HttpStatusCode.Ok,
-      }
     );
   } catch (ex) {
     return NextResponse.json(
