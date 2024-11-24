402f3d88afc223d24c43dda79910ad3439f5748d
DaDevFox <53617853+DaDevFox@users.noreply.github.com>
2024-11-24 14:48:59 -0500

    Fix minor type error

--- src/app/api/user_courses/[id]/route.ts
+++ src/app/api/user_courses/[id]/route.ts
@@ -81,7 +81,7 @@ export async function DELETE(
 ) {
   try {
     const user_id = (await context.params).id;
-    const err = await CHECK_ADMIN(user_id); // only admin should be able to unassign work -- users could use this to cheat
+    const err = await CHECK_ADMIN(); // only admin should be able to unassign work -- users could use this to cheat
     if (err) return err;
 
     const { course_id } = await request.json();
