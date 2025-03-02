4f7ed4ae9cb1c57e1aa548126dafc6de2193f0fc
gsarav <gitikasarav@gmail.com>
2025-03-02 15:51:38 -0500

    had to add

--- src/db/index.ts
+++ src/db/index.ts
@@ -11,8 +11,17 @@ export const client = new Client({
   database: "mitre_db",
 });
 
-export const connectDB = async () => await client.connect();
+// export const connectDB = async () => await client.connect();
+// export const db = drizzle(client, { schema });
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected to the database successfully");
+  } catch (error) {
+    console.log("FAILED TO CONNECT TO DATABASE", error);
+  }
+}
 
 export const db = drizzle(client, { schema });
-
 connectDB();
