6c92e131e4dc0f20be41712c480d1d20fc56cceb
Anvay Panguluri <aspanguluri@gmail.com>
2025-04-15 13:01:57 -0400

    linter

--- src/app/api/auth/reset_password/clear_codes/route.ts
+++ src/app/api/auth/reset_password/clear_codes/route.ts
@@ -3,18 +3,16 @@ import { HttpStatusCode } from "axios";
 import { NextResponse } from "next/server";
 
 export async function DELETE() {
-    try {
-        await clearExpiredCodes();
-        
-        const response = NextResponse.json(
-            { status: HttpStatusCode.Ok },
-        );
+  try {
+    await clearExpiredCodes();
 
-        return response;
-    } catch (error) {
-        return NextResponse.json(
-            { error: "Only admin may delete codes." },
-            { status: HttpStatusCode.Forbidden }
-        );
-    }
+    const response = NextResponse.json({ status: HttpStatusCode.Ok });
+
+    return response;
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Only admin may delete codes." },
+      { status: HttpStatusCode.Forbidden }
+    );
+  }
 }
