bc0ca54d13a195182ba2c1fdc579fdc373a8be23
Anvay Panguluri <aspanguluri@gmail.com>
2025-04-19 19:19:35 -0400

    Forgot password feature done

--- src/app/admin/dashboard/validation-codes-popup.tsx
+++ src/app/admin/dashboard/validation-codes-popup.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Button } from "@/components/ui/button";
 import {
   DialogClose,
@@ -6,69 +7,58 @@ import {
   DialogTitle,
 } from "@radix-ui/react-dialog";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { HttpStatusCode } from "axios";
 import React from "react";
 
 type ValidationCode = {
-  email: string;
+  user_email: string;
   code: string;
-  expirationTime: Date;
+  expiration_time: string;
 };
 
 const ValidationCodesPopup = () => {
-  const [validationCodes, setValidationCodes] = React.useState<
-    ValidationCode[]
-  >([
-    {
-      email: "user1@example.com",
-      code: "ABC123",
-      expirationTime: new Date(Date.now() + 3600000),
-    },
-    {
-      email: "user2@example.com",
-      code: "DEF456",
-      expirationTime: new Date(Date.now() + 3600000),
-    },
-    {
-      email: "user3@example.com",
-      code: "GHI789",
-      expirationTime: new Date(Date.now() + 3600000),
-    },
-    {
-      email: "user4@example.com",
-      code: "JKL012",
-      expirationTime: new Date(Date.now() + 3600000),
-    },
-    {
-      email: "user5@example.com",
-      code: "MNO345",
-      expirationTime: new Date(Date.now() + 3600000),
-    },
-    {
-      email: "user6@example.com",
-      code: "PQR678",
-      expirationTime: new Date(Date.now() + 3600000),
-    },
-    {
-      email: "user7@example.com",
-      code: "STU901",
-      expirationTime: new Date(Date.now() + 3600000),
-    },
-    {
-      email: "user8@example.com",
-      code: "VWX234",
-      expirationTime: new Date(Date.now() + 3600000),
-    },
-    {
-      email: "user9@example.com",
-      code: "YZA567",
-      expirationTime: new Date(Date.now() + 3600000),
-    },
-    {
-      email: "user10@example.com",
-      code: "BCD890",
-      expirationTime: new Date(Date.now() + 3600000),
-    },
-  ]);
+  const [validationCodes, setValidationCodes] = React.useState<ValidationCode[]>([]);
+  const [loading, setLoading] = React.useState(true);
+  const [errorMessage, setErrorMessage] = React.useState("");
+
+  const fetchCodes = async () => {
+    try {
+      const res = await fetch("/api/auth/reset_password/get_codes", {
+        method: "POST",
+        body: JSON.stringify({}),
+      });
+      const data = await res.json();
+
+      const parsedData: ValidationCode[] = data.data.map((code: any) => ({
+        ...code,
+        expirationTime: new Date(code.expirationTime).toString(),
+      }));
+
+      setValidationCodes(parsedData);
+    } catch (err) {
+      setErrorMessage("Failed to fetch codes.")
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDelete = async () => {
+    const resp = await fetch("/api/auth/reset_password/clear_codes", {
+      method: "DELETE"
+    })
+
+    if (resp.status != HttpStatusCode.Ok) {
+      setErrorMessage("An error occurred while clearing the expired codes.")
+    }
+
+    setLoading(true);
+    await fetchCodes();
+
+  };
+
+  React.useEffect(() => {
+    fetchCodes();
+  }, []);
 
   return (
     <div className="">
@@ -78,16 +68,20 @@ const ValidationCodesPopup = () => {
       <DialogTitle>Validation Codes</DialogTitle>
       <div className="mb-4 flex items-center justify-between">
         <DialogDescription className="text-sm text-darkLight">
-          Provide these codes to users to reset their passwords
+          Provide these codes to users to reset their passwords.
         </DialogDescription>
 
         <Button
           className="rounded-md bg-slate-800 text-sm text-white hover:bg-slate-600"
-          onClick={() => {}}
+          onClick={handleDelete}
         >
           Clear Expired Codes
         </Button>
       </div>
+
+      {errorMessage && (
+        <p className="text-customRed mb-4">{errorMessage}</p>
+      )}
 
       <div className="my-2 overflow-hidden rounded-md border border-gray-300">
         <table className="w-full">
@@ -101,19 +95,35 @@ const ValidationCodesPopup = () => {
             </tr>
           </thead>
           <tbody className="text-base text-darkLight">
-            {validationCodes.map((code, index) => (
-              <tr key={index}>
-                <td className="border-b border-gray-300 px-6 py-1">
-                  {code.email}
-                </td>
-                <td className="border-b border-gray-300 px-6 py-1">
-                  {code.code}
-                </td>
-                <td className="border-b border-gray-300 px-6 py-1">
-                  {code.expirationTime.toLocaleString()}
+            {loading ? (
+              <tr>
+                <td colSpan={3} className="text-center py-4">
+                  Loading...
                 </td>
               </tr>
-            ))}
+            ) : (
+              validationCodes.map((code, index) => {
+                const isExpired = new Date(code.expiration_time) < new Date();
+
+                const cellClass = `border-b border-gray-300 px-6 py-1 ${
+                  isExpired ? "text-red": ""
+                }`
+
+                return (
+                  <tr key={index}>
+                    <td className={cellClass}>
+                      {code.user_email}
+                    </td>
+                    <td className={cellClass}>
+                      {code.code}
+                    </td>
+                    <td className={cellClass}>
+                      {new Date(code.expiration_time).toLocaleString()}
+                    </td>
+                  </tr>
+                );
+              })
+            )}
           </tbody>
         </table>
       </div>
